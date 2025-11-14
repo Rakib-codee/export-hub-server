dc7b6e7ea60ddcce030651513fb44f3e38b924de
Md Mahfujur Rahman Rakib <rakibislam4913@gmail.com>
2025-11-14 19:52:33 +0800

    fix error for deployment

--- index.js
+++ index.js
@@ -244,7 +244,7 @@ async function run() {
       }
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensure client will close when you finish/error
